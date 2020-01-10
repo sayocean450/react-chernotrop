6746e6791eb10d4cf108e71d55fc90ff0d5246f9
Artem S <sayocean450@gmail.com>
2020-01-10 10:14:54 +0300

    Add Manifest text

--- src/components/Manifest/Manifest.jsx
+++ src/components/Manifest/Manifest.jsx
@@ -2,12 +2,13 @@ import React, {Component} from 'react';
 import './Manifest.scss';
 import MainMenu from '../MainMenu/MainMenu';
 import Main from '../Main/Main';
+import MainManifestText from '../MainManifestText/MainManifestText';
 
 class Manifest extends Component {
   render() {
     return (
       <div className="Manifest">
-        <Main left={<p>Левый</p>}
+        <Main left={<MainManifestText/>}
               right={<MainMenu active="manifest"/>}/>
       </div>
     );
