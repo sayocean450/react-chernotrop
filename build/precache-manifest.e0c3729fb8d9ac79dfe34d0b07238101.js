83801017522be49e649cb53bfa4c59574535352a
Artem S <sayocean450@gmail.com>
2020-01-10 10:40:52 +0300

    Add build path

---
+++ build/precache-manifest.e0c3729fb8d9ac79dfe34d0b07238101.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b197ab16877ebc14c5c93595d511c58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "946accae8ea76d76ddcf",
+    "url": "/static/css/main.9e0c20d3.chunk.css"
+  },
+  {
+    "revision": "eb0376d4af05e56f363a",
+    "url": "/static/js/2.65714493.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.65714493.chunk.js.LICENSE"
+  },
+  {
+    "revision": "946accae8ea76d76ddcf",
+    "url": "/static/js/main.767b672b.chunk.js"
+  },
+  {
+    "revision": "b8e78852d4ebba350990",
+    "url": "/static/js/runtime-main.e40b13c4.js"
+  },
+  {
+    "revision": "7739b965aaefa68910f57b188e7c58a7",
+    "url": "/static/media/arkhip-regular.7739b965.otf"
+  },
+  {
+    "revision": "838b71c3b718b1c473ebcdbb5261b8ae",
+    "url": "/static/media/arkhip-regular.838b71c3.ttf"
+  },
+  {
+    "revision": "0cc1580afb40ac9966c73e4b66846e22",
+    "url": "/static/media/carrots-rotterdam.0cc1580a.png"
+  },
+  {
+    "revision": "1a30082581039685ec40eab9a68a9e2c",
+    "url": "/static/media/pt-root-ui-bold.1a300825.eot"
+  },
+  {
+    "revision": "322151afe6ce40fd9fc91607696cdc4a",
+    "url": "/static/media/pt-root-ui-bold.322151af.ttf"
+  },
+  {
+    "revision": "ee168f16fed9c73504698df2ee951517",
+    "url": "/static/media/pt-root-ui-bold.ee168f16.woff2"
+  },
+  {
+    "revision": "7c705bf82ec224bb8477351b548bc642",
+    "url": "/static/media/pt-root-ui-regular.7c705bf8.ttf"
+  },
+  {
+    "revision": "99c150c79c3d4a021256a9446c7b2937",
+    "url": "/static/media/pt-root-ui-regular.99c150c7.woff2"
+  },
+  {
+    "revision": "f7552d29847c3c960359918481d7db43",
+    "url": "/static/media/pt-root-ui-regular.f7552d29.eot"
+  },
+  {
+    "revision": "f3c93e64210659816752d5bfedc96dcf",
+    "url": "/static/media/section-pogodin.f3c93e64.png"
+  }
+]);
